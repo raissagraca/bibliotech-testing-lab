40341b454bf90cc5f3b1761007cfbc871a247f16
Raíssa da Graça <raissagraca.rdsantos@gmail.com>
2026-08-31 20:08:16 -0300

    Add files via upload

---
+++ bibliotech.py
@@ -0,0 +1,29 @@
+LIMITE_EMPRESTIMOS = 3
+
+def pode_emprestar(usuario_ativo, possui_pendencia, emprestimos_ativos):
+    if not usuario_ativo:
+        return False
+    if possui_pendencia:
+        return False
+    # Defeito proposital da atividade: a regra exige menos de 3.
+    if emprestimos_ativos > LIMITE_EMPRESTIMOS:
+        return False
+    return True
+
+def calcular_multa(dias_atraso):
+    if dias_atraso <= 0:
+        return 0.0
+    if dias_atraso <= 7:
+        return dias_atraso * 2.0
+    dias_excedentes = dias_atraso - 7
+    return 14.0 + (dias_excedentes * 3.0)
+
+def classificar_atraso(dias_atraso):
+    if dias_atraso <= 0:
+        return "sem atraso"
+    elif dias_atraso <= 7:
+        return "atraso leve"
+    elif dias_atraso <= 30:
+        return "atraso moderado"
+    else:
+        return "atraso grave"
